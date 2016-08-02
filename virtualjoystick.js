b6d6a5619f4c28fabe91d66cf45522ac5f218202
ssugar <scottsugar@outlook.com>
2016-08-02 22:17:22 +0700

    setting stick X and Y to base X and Y in _onUp when stationarybase: true

--- virtualjoystick.js
+++ virtualjoystick.js
@@ -155,6 +155,9 @@ VirtualJoystick.prototype._onUp	= function()
 	
 		this._baseX	= this._baseY	= 0;
 		this._stickX	= this._stickY	= 0;
+	}else{
+		this._stickX	= this._baseX;
+		this._stickY	= this._baseY;
 	}
 }
 
